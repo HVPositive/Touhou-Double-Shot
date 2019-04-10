7804821bf7d3ea8757ad8c37d7007380a9a08b53
Henry Vo <henryvo62@gmail.com>
2019-04-10 02:15:46 -0400

    initial commit

---
+++ Touhou Double Shot/Assets/Scripts/GameControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+	//public Text[] buttonList;
+	private string playerSide;
+
+	void Awake(){
+
+		playerSide = "1";
+	}
+
+	public string GetPlayerSide(){
+		return "1";
+	}
+
+	public void SetPlayerSide(String newPlayer){
+		return newPlayer;
+	}
+}
---
+++ Touhou Double Shot/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour {
+
+	public Button button;
+	public Text buttonText;
+
+
+	private GameControl gameControl;
+
+
+	public void  SetGameControlReference(GameControl control){
+		gameControl = control;
+	}
+
+
+	public void SetSpace(){
+
+		if (gameControl.GetPlayerSide() == "1"){
+
+
+			buttonText.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("reimu/large");
+			gameControl.SetPlayerSide("2");
+		}
+		else{ 
+			buttonText.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("marisa/large");
+			gameControl.SetPlayerSide("1");
+		}
+
+		button.interactable = false;
+	}
+}
