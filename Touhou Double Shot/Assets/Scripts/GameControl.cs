313d69f724bc79b7a880f38272a494460421dc3f
Henry Vo <henryvo62@gmail.com>
2019-06-30 04:28:50 -0400

    Created item class for dictionaries. Split dictionary into one dictionary for each player. Now checks hits depending on current player turn. Detects hits in all directions.

--- Touhou Double Shot/Assets/Scripts/GameControl.cs
+++ Touhou Double Shot/Assets/Scripts/GameControl.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class GameControl : MonoBehaviour {
 
 	public Text[] buttonList;
 	private string playerSide;
 	private int boardCounter;
-	private Dictionary<string,string> locations;
+	private Dictionary<string,Items.Item> p1Locations;
+	private Dictionary<string,Items.Item> p2Locations;
+
+	private string p1Char;
+	private string p2Char;
 
 	private void Start(){
 
 		boardCounter = 0;
 		playerSide = "0";
 
-		locations = new Dictionary<string,string>();
+		p1Locations = new Dictionary<string,Items.Item>();
+		p2Locations = new Dictionary<string,Items.Item>();
+
+		p1Char = "reimu";
+		p2Char = "marisa";
+
 	}
 
 	public string GetPlayerSide(){
@@ -34,11 +44,24 @@ public class GameControl : MonoBehaviour {
 		return boardCounter;
 	}
 
-	public void AddLocation(string gridspace, string item){
-		locations.Add(gridspace, item);
+	public void AddLocation(string gridspace, Items.Item newI, int playerNum){
+		GetLocations(playerNum).Add(gridspace, newI);
 	}
-	public Dictionary<string,string> GetLocations(){
-		return locations;
+	public Dictionary<string,Items.Item> GetLocations(int playerNum){
+		if (playerNum == 1)
+			return p1Locations;
+		else if (playerNum == 2)
+			return p2Locations;
+		else 
+			return new Dictionary<string,Items.Item>(); //empty list
+	}
+
+	public string GetP1Char(){
+		return p1Char;
+	}
+
+	public string GetP2Char(){
+		return p2Char;
 	}
 
 }
--- Touhou Double Shot/Assets/Scripts/GridSpace.cs
+++ Touhou Double Shot/Assets/Scripts/GridSpace.cs
@@ -10,6 +10,7 @@ public class GridSpace : MonoBehaviour {
 	private SpriteRenderer spriteRend;
 
 
+
 	public GameControl gameControl;
 
 
@@ -37,18 +38,18 @@ public class GridSpace : MonoBehaviour {
 	public void SetSpace(){
 		//Sets up the board depending on selected space.
 		if (gameControl.GetBoardCounter() == 0){
-			SetSprite("reimu", "large");
+			SetSprite(1, "large");
 		}
 		else if (gameControl.GetBoardCounter() == 1)
-			SetSprite("reimu", "medium");
+			SetSprite(1, "medium");
 		else if (gameControl.GetBoardCounter() == 2)
-			SetSprite("reimu", "small");
+			SetSprite(1, "small");
 		else if (gameControl.GetBoardCounter() == 3)
-			SetSprite("marisa", "large");			
+			SetSprite(2, "large");			
 		else if (gameControl.GetBoardCounter() == 4)
-			SetSprite("marisa", "medium");
+			SetSprite(2, "medium");
 		else if (gameControl.GetBoardCounter() == 5)
-			SetSprite("marisa", "small");
+			SetSprite(2, "small");
 
 		//only increment if all set. Afterwards, this listener is usless and should be removed if activated.
 		if (gameControl.GetBoardCounter() < 6)
@@ -60,42 +61,72 @@ public class GridSpace : MonoBehaviour {
 	}
 
 	
-	public void SetSprite(string character, string size){
-		spriteRend.sprite = Resources.Load<Sprite>(character + "/" + size);
+	public void SetSprite(int charNum, string size){
+		string charName = GetCharName(charNum);
+		spriteRend.sprite = Resources.Load<Sprite>(charName + "/" + size);
 		//gameControl.AddLocation(character + " " + size, this.name);
-		gameControl.AddLocation(this.name, character + " " + size);
+
+		gameControl.AddLocation(this.name, new Items.Item(size,charName), charNum);
 
 
 
 	}
 
+	//even is p1, odd is p2 turn
+
 	public void CheckHit(){
 		if (gameControl.GetBoardCounter() < 6){
 			return;
 		}
-		if (gameControl.GetLocations().ContainsKey(this.name) ){
+
+		int pNum = 0;
+
+
+		if (gameControl.GetBoardCounter() % 2 == 0)
+			pNum = 2; //If even, then p1's turn and check p2
+		else
+			pNum = 1; 
+
+		if (gameControl.GetLocations(pNum).ContainsKey(this.name) ){
 			Debug.Log("nice hit");
-		} else if (CheckGraze()){
+		} else if (CheckGraze(pNum)){
 			Debug.Log("nice graze");
 		} else{
 			Debug.Log("nice miss");
 		}
 
+		gameControl.IncBoardCounter();
+
 	} 	
 
 
-	public bool CheckGraze(){
+	public bool CheckGraze(int pNum){
+		bool hitFound = false;
 
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+		RaycastHit2D hit;
+		Vector2[] grazeVectors = new Vector2[] { Vector2.up, new Vector2(1,1), Vector2.right,
+		 										 new Vector2(1,-1), Vector2.down, new Vector2(-1,-1),
+		 										 Vector2.left, new Vector2(-1,1),Vector2.right};
 
-		if (gameControl.GetLocations().ContainsKey(hit.collider.gameObject.name)){
+		for (int i =0; i < grazeVectors.Length; i++){
+			hit = Physics2D.Raycast(transform.position, grazeVectors[i]);
 
-			return true;
+			if (hit.collider!=null && gameControl.GetLocations(pNum).ContainsKey(hit.collider.gameObject.name))
+				hitFound = true;
 
-		}else{
-			return false;
 		}
 
+		return hitFound;
 
+
+	}
+
+	public string GetCharName(int num){
+		if (num ==1)
+			return gameControl.GetP1Char();
+		else if (num ==2)
+			return gameControl.GetP2Char();
+		else
+			return "reimu"; //default char?
 	}
 }
