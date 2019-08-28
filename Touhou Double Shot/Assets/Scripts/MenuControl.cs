e0e6e4722df172c54d6bd03abb0ab2912acf4176
HVPositive <henryvo62@gmail.com>
2019-08-28 00:53:53 -0400

    Started game setup screen. Menu buttons can now navigate through screens. Removed text component from spaces since they are not needed.

--- Touhou Double Shot/Assets/Scripts/MenuControl.cs
+++ Touhou Double Shot/Assets/Scripts/MenuControl.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 
 public class MenuControl : MonoBehaviour {
 
+	
+	public List<GameObject> allMenus;
+	public GameObject mainMenu;
+	public GameObject playSettings;
+
+
+	private void Start(){
+		ReturnToMainMenu();
+	}
+
+	public void ReturnToMainMenu(){
+		ShowMenu(mainMenu);
+		Debug.Log("test?");
+	}
+
+	public void OpenGameSetup(){
+		ShowMenu(playSettings);
+		Debug.Log("test2");
+	}
+
 	public void QuitGame(){
 		
 		Debug.Log("Quit");
 		Application.Quit();
+
+	}
+
+	public void ShowMenu(GameObject s){
+		foreach (var m in allMenus)
+			m.SetActive(false);
+
+		s.SetActive(true);
 
 	}
 }
